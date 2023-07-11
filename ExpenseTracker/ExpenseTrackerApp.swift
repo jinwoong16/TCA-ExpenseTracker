c9a2d450257f0ef52aa6ccfb6e83f34a1829039c
jinwoong <baseball0020@gmail.com>
2023-07-11 13:06:21 +0900

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by jinwoong Kim on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
